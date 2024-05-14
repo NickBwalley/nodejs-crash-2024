0180138961bd5b0479492f451961b983b25c2d60
NickBwalley <nickbwalley@gmail.com>
2024-05-14 10:12:07 +0300

    precommit

---
+++ index.js
@@ -0,0 +1,4 @@
+const { generateRandomNumber, celciusToFarenheit } = require("./utils");
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(`Celcius to Farenheit: ${celciusToFarenheit(0)}`);
